35d07a83cea61f762cb9a951badba8dec1454807
erik <erikborsos21@gmail.com>
2022-06-06 20:09:36 +0200

    :memo: Added impress and data protection

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -9,15 +9,21 @@ const Footer = () => (
     mr={'auto'}
     left={4}
     right={4}
-    textAlign='center'
-  >
+    textAlign='center'>
     <Box textAlign='left' position='absolute' left={0}>
       &copy; {new Date().getFullYear()} Erik Borsos. All rights reserved.
     </Box>
     <Box textAlign='right' right={0}>
-      <Link href='/impressum' variant='colored'>Impressum</Link>
+      <Link href='/impressum' variant='colored'>
+        Impressum
+      </Link>
       {' | '}
-      <Link href='/datenschutz' variant='colored'>Datenschutz</Link>
+      <Link
+        download
+        href='/assets/documents/Datenschutzerklaerung.pdf'
+        variant='colored'>
+        Datenschutzerklärung
+      </Link>
     </Box>
   </Box>
 )
